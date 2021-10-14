525ea38b20a49f10f721d7ca179af61980452304
Momin94 <momin.shoaib@cheetay.pk>
2021-10-14 17:14:35 +0500

    further code cleanup

--- Movies/Utilities/Observable.swift
+++ Movies/Utilities/Observable.swift
@@ -1,20 +1,17 @@
 final class Observable<T> {
-    // 1
     typealias Listener = (T) -> Void
     var listener: Listener?
-    // 2
+
     var value: T {
         didSet {
             listener?(value)
         }
     }
 
-    // 3
     init(_ value: T) {
         self.value = value
     }
 
-    // 4
     func bind(listener: Listener?) {
         self.listener = listener
         listener?(value)
